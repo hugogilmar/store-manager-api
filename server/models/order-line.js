7bacb1e531317310e047a91830cf66dcd69ab50e
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-03 15:17:27 -0600

    products and product categories report added

--- server/models/order-line.js
+++ server/models/order-line.js
@@ -3,6 +3,38 @@
 module.exports = function(OrderLine) {
   const app = require('../../server/server');
 
+  OrderLine.productsReport = function(storeId, dateFrom, dateTo, callback) {
+    var query = "SELECT p.code, p.name, SUM(ol.quantity) AS quantity, SUM(ol.total) AS total FROM `OrderLine` ol LEFT JOIN `Order` o ON ol.orderId = o.id LEFT JOIN `Product` p ON ol.productId = p.id WHERE o.storeId = ? AND o.date >= ? AND o.date <= ? GROUP BY p.code, p.name ORDER BY quantity DESC";
+    var params = [storeId, dateFrom, dateTo];
+    var data = [];
+
+    app.dataSources.database.connector.execute(query, params, function(error, rows) {
+      if (error) {
+        console.log(error);
+      } else {
+        data = rows;
+      }
+
+      callback(null, data);
+    });
+  };
+
+  OrderLine.productCategoriesReport = function(storeId, dateFrom, dateTo, callback) {
+    var query = "SELECT pc.code, pc.name, SUM(ol.quantity) AS quantity, SUM(ol.total) AS total FROM `OrderLine` ol LEFT JOIN `Order` o ON ol.orderId = o.id LEFT JOIN `Product` p ON ol.productId = p.id LEFT JOIN `ProductCategory` pc ON p.productCategoryId = pc.id WHERE o.storeId = ? AND o.date >= ? AND o.date <= ? GROUP BY pc.code, pc.name ORDER BY quantity DESC";
+    var params = [storeId, dateFrom, dateTo];
+    var data = [];
+
+    app.dataSources.database.connector.execute(query, params, function(error, rows) {
+      if (error) {
+        console.log(error);
+      } else {
+        data = rows;
+      }
+
+      callback(null, data);
+    });
+  };
+
   OrderLine.prototype.calculateTotals = function (callback, autosave = false) {
     let self = this;
     let subtotal = 0;
