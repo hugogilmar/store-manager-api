59b8def23c03debdd86458aa969362ddb01118ab
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-03 15:17:42 -0600

    payment methods report added

--- server/models/order.js
+++ server/models/order.js
@@ -1,6 +1,24 @@
 'use strict';
 
 module.exports = function(Order) {
+  const app = require('../../server/server');
+
+  Order.paymentMethodsReport = function(storeId, dateFrom, dateTo, callback) {
+    var query = "SELECT p.code, p.name, COUNT(*) AS quantity, SUM(i.amount) AS total FROM `Invoice` i LEFT JOIN `Order` o ON i.orderId = o.id LEFT JOIN `PaymentMethod` p ON i.paymentMethodId = p.id WHERE o.storeId = ? AND o.date >= ? AND o.date <= ? GROUP BY p.code, p.name ORDER BY quantity DESC";
+    var params = [storeId, dateFrom, dateTo];
+    var data = [];
+
+    app.dataSources.database.connector.execute(query, params, function(error, rows) {
+      if (error) {
+        console.log(error);
+      } else {
+        data = rows;
+      }
+
+      callback(null, data);
+    });
+  };
+
   Order.prototype.calculateTotals = function (callback, autosave = false) {
     let self = this;
     let subtotal = 0;
