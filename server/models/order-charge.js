4c9164cfb596003b72cd49fe4b13453dcc136609
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-06 04:54:54 -0600

    charges calculation

--- server/models/order-charge.js
+++ server/models/order-charge.js
@@ -1,5 +1,32 @@
 'use strict';
 
 module.exports = function(OrderCharge) {
+  const app = require('../../server/server');
 
+  OrderCharge.prototype.calculateTotals = function (callback, autosave = false) {
+    let self = this;
+    let amount = 0;
+
+    app.models.Charge.findOne({ where: { id: this.chargetId } }).then(function (charge) {
+      amount = charge.amount;
+
+      self.amount = amount;
+
+      if (autosave) {
+        self.save();
+      }
+
+      callback();
+    });
+  }
+
+  OrderCharge.observe('before save', function (ctx, next) {
+    ctx.instance.calculateTotals(next);
+  });
+
+  OrderCharge.observe('after save', function (ctx, next) {
+    app.models.Order.findOne({ where: { id: ctx.instance.orderId } }).then(function (order) {
+      order.calculateTotals(next, true);
+    });
+  });
 };
