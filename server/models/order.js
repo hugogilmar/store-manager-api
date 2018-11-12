f46104cc1f1374fe22dd65d893249907d36f9977
Hugo Gilmar Erazo <hugogilmar@gmail.com>
2018-11-11 22:23:28 -0600

    stores report added

--- server/models/order.js
+++ server/models/order.js
@@ -3,9 +3,9 @@
 module.exports = function(Order) {
   const app = require('../../server/server');
 
-  Order.paymentMethodsReport = function(storeId, dateFrom, dateTo, callback) {
-    var query = "SELECT p.code, p.name, COUNT(*) AS quantity, SUM(i.amount) AS total FROM `Invoice` i LEFT JOIN `Order` o ON i.orderId = o.id LEFT JOIN `PaymentMethod` p ON i.paymentMethodId = p.id WHERE o.storeId = ? AND o.date >= ? AND o.date <= ? GROUP BY p.code, p.name ORDER BY quantity DESC";
-    var params = [storeId, dateFrom, dateTo];
+  Order.storesReport = function(dateFrom, dateTo, callback) {
+    var query = "SELECT s.name, COUNT(*) AS quantity, SUM(o.total) AS total FROM `Order` o LEFT JOIN `Store` s ON o.storeId = s.id WHERE o.date >= ? AND o.date <= ? GROUP BY s.name ORDER BY quantity DESC";
+    var params = [dateFrom, dateTo];
     var data = [];
 
     app.dataSources.database.connector.execute(query, params, function(error, rows) {
